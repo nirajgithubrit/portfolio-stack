538b46df98bf97d8fb66b5fa28b25b523edc4019
niraj-kumar2 <126452817+niraj-kumar2@users.noreply.github.com>
2026-05-05 21:22:47 +0530

    fix: production config

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,5 +2,11 @@ import mongoose from 'mongoose';
 
 export async function connectDb(uri: string): Promise<void> {
   mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
+  try{
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+  }catch(error){
+    console.error('Error connecting to MongoDB', error);
+    process.exit(1);
+  }
 }
